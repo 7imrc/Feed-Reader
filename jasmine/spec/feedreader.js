cb826b34a3ff65499eca55a2244645cb1a675830
Tim Campbell <tim.campbell@btinternet.com>
2018-07-23 17:33:18 +0100

    Added 'Initial Entries' test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -76,8 +76,8 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
+    /* This test suite determines that new feeds load correctly */
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -91,4 +91,6 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+    });
 }());
